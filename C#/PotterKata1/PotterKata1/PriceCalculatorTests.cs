bc55d9a9b97deea2202bd6ef47e01e256d5e4451
Grzegorz Dziadkiewicz <zwei1666@gmail.com>
2015-08-04 17:24:31 +0200

    5. Red

--- C#/PotterKata1/PotterKata1/PriceCalculatorTests.cs
+++ C#/PotterKata1/PotterKata1/PriceCalculatorTests.cs
@@ -10,6 +10,7 @@ namespace PotterKata1
         [TestCase(3*8*0.9, 1, 2, 3)]
         [TestCase(4*8*0.8, 1, 2, 3, 4)]
         [TestCase(5*8*0.75, 1, 2, 3, 4, 5)]
+        [TestCase(5*8*0.75 + 8, 1, 2, 3, 4, 5, 5)]
         public void ShouldReturnProperPriceForVariousCombinationsOfBooks(double expectedPrice, params int [] books)
         {
            // When
