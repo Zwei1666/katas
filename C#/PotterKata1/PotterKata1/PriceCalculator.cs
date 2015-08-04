a65c5538aebc8b2e922cd6a3569286e8c0dcea1e
Grzegorz Dziadkiewicz <zwei1666@gmail.com>
2015-08-04 16:49:30 +0200

    2. Refactor #2

---
+++ C#/PotterKata1/PotterKata1/PriceCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace PotterKata1
+{
+    public static class PriceCalculator
+    {
+        private const double BaseBookPrice = 8.0;
+        public static double CalculatePrice(params int [] books) => BaseBookPrice * books.Length;
+    }
+}
--- C#/PotterKata1/PotterKata1/PriceCalculatorTests.cs
+++ C#/PotterKata1/PotterKata1/PriceCalculatorTests.cs
@@ -2,12 +2,6 @@
 
 namespace PotterKata1
 {
-    public static class PriceCalculator
-    {
-        private const double BaseBookPrice = 8.0;
-        public static double CalculatePrice(params int [] books) => BaseBookPrice * books.Length;
-    }
-
     public class PriceCalculatorTests
     {
         [Test]
