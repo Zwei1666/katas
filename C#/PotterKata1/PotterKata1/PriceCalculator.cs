a201210cabab4af70b68f3790e3784c31fc4f976
Grzegorz Dziadkiewicz <zwei1666@gmail.com>
2015-08-04 18:03:03 +0200

    5. Green

--- C#/PotterKata1/PotterKata1/PriceCalculator.cs
+++ C#/PotterKata1/PotterKata1/PriceCalculator.cs
@@ -1,17 +1,28 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Serialization.Formatters;
 
 namespace PotterKata1
 {
     public static class PriceCalculator
     {
         private const double BaseBookPrice = 8.0;
-        public static double CalculatePrice(params int [] books)
-        {
-            var price = BaseBookPrice * books.Length;
-            var distinctBooks = books.Distinct().Count();
-            var discount = GetDiscountFor(distinctBooks);
 
-            return price * discount;
+        public static double CalculatePrice(params int[] books)
+        {
+            var numOfDifferentKindsOfBooks =
+                books.GroupBy(book => book).Select(sameBooks => sameBooks.Count()).OrderBy(numOfBooks => numOfBooks);
+            var setsOfBooks = numOfDifferentKindsOfBooks.Aggregate(new List<int>(),
+                (sets, i) =>
+            {
+                sets.Add(i - sets.Sum());
+                return sets;
+            }
+            );
+            var price = setsOfBooks.Select((set ,i) => set*BaseBookPrice* (numOfDifferentKindsOfBooks.Count()-i)*GetDiscountFor(numOfDifferentKindsOfBooks.Count() - i)).Sum();
+
+            return price;
         }
 
         private static double GetDiscountFor(int distinctBooks)
