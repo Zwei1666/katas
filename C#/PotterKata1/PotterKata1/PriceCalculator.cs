3e85e01f01434de8738d4216b23fc35f7a8147b3
Grzegorz Dziadkiewicz <zwei1666@gmail.com>
2015-08-04 16:56:01 +0200

    3. Green

--- C#/PotterKata1/PotterKata1/PriceCalculator.cs
+++ C#/PotterKata1/PotterKata1/PriceCalculator.cs
@@ -1,8 +1,13 @@
-﻿namespace PotterKata1
+﻿using System.Linq;
+
+namespace PotterKata1
 {
     public static class PriceCalculator
     {
         private const double BaseBookPrice = 8.0;
-        public static double CalculatePrice(params int [] books) => BaseBookPrice * books.Length;
+        public static double CalculatePrice(params int [] books)
+        {
+            return BaseBookPrice * books.Length * (books.Distinct().Count() >1?0.95:1);
+        }
     }
 }
