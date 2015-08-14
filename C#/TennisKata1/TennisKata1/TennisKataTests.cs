44e2b4953eaa9479a86d011ca982d24b62113e0a
meoke <paulina-ph@wp.pl>
2015-08-14 18:09:29 +0200

    1 Red

---
+++ C#/TennisKata1/TennisKata1/TennisKataTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Media;
+using System.Security.Policy;
+using NUnit.Framework;
+
+namespace TennisKata1
+{
+    public class TennisKataTests
+    {
+        [Test]
+        public void CurrentScoreShouldBeDisplayedProperly()
+        {
+            //Given
+            var game = new Game();
+            var expectedResult = "love:love";
+
+            //When
+            game.PlayerA.SetPoints(0);
+            game.PlayerB.SetPoints(0);
+            var result = game.Score;
+
+            //Then
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
