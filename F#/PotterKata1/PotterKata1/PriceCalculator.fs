debfc12708a2d48d9728aeb26f9946acbe663896
Grzegorz Dziadkiewicz <zwei1666@gmail.com>
2015-08-16 19:16:17 +0200

    Potter kata in F#

---
+++ F#/PotterKata1/PotterKata1/PriceCalculator.fs
@@ -0,0 +1,29 @@
+﻿module PotterKata1.PriceCalculator
+let baseBookPrice = 8.0
+
+let findOrDefault x set =
+     match set |> Seq.tryFind (fun (x',_) -> x' = x) with
+        |Some (_, y) -> y
+        |None -> 0
+
+let replace3and5ForTwo4 (set:(int*int) seq) =
+    let numberOf3 = set |> findOrDefault 3
+    let numberOf4 = set |> findOrDefault 4
+    let numberOf5 = set |> findOrDefault 5
+    let numberOfReplacements = min numberOf3 numberOf5
+    set |> Seq.filter (fun (x,_) -> x<3 ||x >5) |> Seq.append [(3,numberOf3 - numberOfReplacements); (4,numberOf4+2*numberOfReplacements); (5, numberOf5-numberOfReplacements)]
+
+let calculatePrice(books:array<int>) =
+    let disscount numOfDistinctBooks = 
+        match numOfDistinctBooks with
+        |2 -> 0.95
+        |3 -> 0.9
+        |4 -> 0.8
+        |5 -> 0.75
+        |_ -> 1.0
+    let distinctBooksSetPrice cardinality = float cardinality * baseBookPrice * disscount cardinality 
+    let countsOfDistinctBooks = books |> Seq.countBy (fun x -> x) |> Seq.map snd  |> Seq.append [0] |> Seq.sortBy (fun x -> -x) 
+    let numOfSetsWithDifferentCardinality = countsOfDistinctBooks |> Seq.pairwise |> Seq.mapi (fun i (x, y) -> (i+1, x-y) )
+    let opimizedNumOfSetsWithDifferentCardinality = replace3and5ForTwo4 numOfSetsWithDifferentCardinality
+    opimizedNumOfSetsWithDifferentCardinality |> Seq.map (fun (setCardinality, numOfSets)->float numOfSets * distinctBooksSetPrice setCardinality) |> Seq.sum
+
---
+++ F#/PotterKata1/PotterKata1/Tests.fs
@@ -0,0 +1,40 @@
+﻿module PotterKata1.Tests
+
+open System
+open PriceCalculator
+open FsUnit
+open NUnit.Framework
+
+let f = Array.reduce (*)
+let trim (x:String) = x.Trim()
+let f2 (s:string) =
+    let skladniki = s.Split '+'
+    let skladniki' = skladniki |> Array.map (fun x -> x.Split '*' |> Array.map trim |> Array.map float) |> Array.map f
+    skladniki' |> Array.sum
+
+type HappyTests() =
+    [<TestCase("0.0", ([||]:int array), TestName = "0 books cost 0")>]
+    [<TestCase("8.0", [|0|], TestName = "1 book costs 8")>]
+    [<TestCase("0.95 * 2.0 * 8.0", [|0; 1|], TestName = "2 different books cost 0.95*2*8")>]
+    [<TestCase("0.9 * 3.0 * 8.0", [|0; 1; 2|], TestName = "3 different books cost 0.9*3*8")>]
+    [<TestCase("0.8 * 4.0 * 8.0", [|0; 1; 2; 3|], TestName = "4 different books cost 0.8*4*8")>]
+    [<TestCase("0.75 * 5.0 * 8.0", [|0; 1; 2; 3; 4|], TestName = "5 different books cost 0.75*5*8")>]
+    [<TestCase("2.0 * 8.0", [|0; 0|], TestName = "2 same books cost 2*8")>]
+    [<TestCase("3.0 * 8.0", [|1; 1; 1|], TestName = "3 same books cost 3*8")>]
+    [<TestCase("8 + 8 * 2 * 0.95", [|0; 0; 1|], TestName = "2 same books and 1 different book cost 8+0.95*2*8")>]
+    [<TestCase("2 * 8 * 2 * 0.95", [|0; 0; 1; 1|], TestName = "2 same books and 2 same different books cost 2*2*8*0.95")>]
+    [<TestCase("8 * 4 * 0.8 + 8 * 2 * 0.95", [|0; 0; 1; 2; 2; 3|], TestName = "4 different books and 2 different books cost 0.8*8*4+0.95*8*2")>]
+    [<TestCase("8 + 8 * 5 * 0.75",[|0; 1; 1; 2; 3; 4|], TestName = "5 different books and one book cost 5*8*0.75 + 8")>]
+    [<TestCase("2 * 8 * 4 * 0.8", [|0; 0; 1; 1; 2; 2; 3; 4|], TestName = "2*8*4*0.8")>]
+    [<TestCase("3 * 8 * 5 * 0.75 + 2 * 8 * 4 * 0.8",
+            [|
+            0; 0; 0; 0; 0; 
+            1; 1; 1; 1; 1;
+            2; 2; 2; 2; 
+            3; 3; 3; 3; 3; 
+            4; 4; 4; 4
+            |], TestName = "2*8*4*0.8 + 3*8*5*0.75")>]
+    member this.``Should properly compute price of different book sets`` price bookSet  =
+        calculatePrice bookSet |> should equal (f2 price)
+  
+        
