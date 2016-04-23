aface3bc9cb1b7315cb2b0d7af6e88ee79c071fa
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2016-04-23 18:08:03 +0200

    Upgrade

--- C#/GenericSum/GenericSum/GenericSigma.cs
+++ C#/GenericSum/GenericSum/GenericSigma.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace GenericSum
 {
     public  class GenericSigma
     {
-        public T Sigma<T>(T start, T stop, Func<T, T> stepFunction, Func<T, T, int> order, Func<T, T, T> additionFunc)
+        public T Sigma<T>(T start, T stop, Func<T> seedFunction, Func<T, T, int> order, Func<T, T> stepFunction, Func<T, T, T> additionFunc)
         {
-            var result = start;
-            for (T i = stepFunction(start); order(i, stop) <= 0; i = stepFunction(i))
+            T result = seedFunction();
+            for (T i = start; order(i, stop) <= 0; i = stepFunction(i))
             {
                 result = additionFunc(result, i);
             }
@@ -24,21 +25,53 @@ namespace GenericSum
         public int SigmaIntegerTest(int start, int stop)
         {
             var sut = new GenericSigma();
-            Func<int, int> stepFunction = (i => i + 1);
+            Func<int, int> stepFunction = i => i + 1;
             Func<int, int, int> orderFunction = (x, y) => x.CompareTo(y);
             Func<int, int, int> additionFunction = (x, y) => x + y;
-            return sut.Sigma(start, stop, stepFunction, orderFunction, additionFunction);
+            return sut.Sigma(start, stop, () => 0, orderFunction, stepFunction, additionFunction);
         }
 
         [TestCase(0.5, 2.5, 0.5 + 1.1 + 1.7 + 2.3)]
         public void SigmaDoubleTest(double start, double stop, double expectedResult)
         {
             var sut = new GenericSigma();
-            Func<double, double> stepFunction = (i => i + 0.6);
+            Func<double, double> stepFunction = i => i + 0.6;
             Func<double, double, int> orderFunction = (x, y) => x.CompareTo(y);
             Func<double, double, double> additionFunction = (x, y) => x + y;
-            var result = sut.Sigma(start, stop, stepFunction, orderFunction, additionFunction);
+            var result = sut.Sigma(start, stop, () => 0.0, orderFunction, stepFunction, additionFunction);
             Assert.AreEqual(expectedResult, result, 0.0000000000001);
+        }
+
+        public class BoxedInt
+        {
+            private int _x;
+
+            public BoxedInt() : this(default(int))
+            {
+                
+            }
+            public BoxedInt(int x)
+            {
+                _x = x;
+            }
+
+            public static BoxedInt operator +(BoxedInt a, BoxedInt b) => new BoxedInt(a._x+b._x);
+
+            public int CompareTo(BoxedInt other) => _x.CompareTo(other._x);
+        }
+
+        [Test]
+        public void ShouldAlwaysReturnNewObject()
+        {
+            var sut = new GenericSigma();
+            Func<BoxedInt, BoxedInt> stepFunction = i => i + new BoxedInt(1);
+            Func<BoxedInt, BoxedInt, int> orderFunction = (x, y) => x.CompareTo(y);
+            Func<BoxedInt, BoxedInt, BoxedInt> additionFunction = (x, y) => x + y;
+            BoxedInt start = new BoxedInt(0);
+            BoxedInt stop = new BoxedInt(0);
+            var result = sut.Sigma(start, stop, () => new BoxedInt(), orderFunction, stepFunction, additionFunction);
+            Assert.AreNotSame(start, result, "is same as start");
+            Assert.AreNotSame(stop, result, " is same as stop");
         }
     }
 }
