a5da2b2f4364e1f0899db0861f7a080b02b99a5e
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2016-07-07 11:36:08 +0200

    F# generic sum

---
+++ F#/Sigma/Sigma/Library1.fs
@@ -0,0 +1,17 @@
+﻿namespace Sigma
+
+module Core =
+    open Microsoft.FSharp.Core
+
+    let sigma(start:^a) (stop:^a) (f : (^a -> ^b) ) : ^b =
+        seq {start .. stop} |> Seq.map f |> Seq.sum
+
+
+module CoreTests =
+    open Xunit
+
+    [<Fact>]
+    let sigma () = 
+    2
+    ()
+
